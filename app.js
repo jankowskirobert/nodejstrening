5fd4cd8d27ea893646e7dc85d7c7fa281e18ef88
rjankowski <robert.jankowski@abastra.com>
2015-08-20 13:59:44 +0200

    add json, ejs, mysql read

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var tests = require('./routes/test');
-//var engine = require('ejs-locals');
+var engine = require('ejs-locals');
 var app = express();
 
 // view engine setup
-//app.engine('ejt', engine);
+app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var mysql      = require('mysql');
+var jsonsafeparse = require('json-safe-parse');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'jkgserver'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('test',{title: 'TEST'});
+//  res.render('test',{title: 'TEST'});
+var tmpQuery = "";
+connection.connect();
+
+connection.query('SELECT * from users', function(err, rows, fields) {
+  if (!err){
+//    	console.log('The solution is: ', rows);
+	for(var i = 0 ; i < rows.length; i++){
+		console.log('USER: ['+i+']', rows[i]);
+	}
+		res.render('test',{title: 'adasdas', user: rows});
+	
+  }else{
+    console.log('Error while performing Query.');
+}
 });
 
+//res.send({
+//title: 'TEST'
+//});
+
+connection.end();
+//connection.release();
+});
 module.exports = router;
